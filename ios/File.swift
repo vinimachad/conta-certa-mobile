c348df0decd7f9b6b969fb67fe490270425406c3
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2022-07-06 08:14:18 -0400

    change from manager to bare workflow and setup watermelondb iOS

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  contacertamobile
+//
+//  Created by Vinicius Galhardo Machado on 05/07/22.
+//
+
+import Foundation
---
+++ ios/contacerta-Briging-Header.h
@@ -0,0 +1,13 @@
+//
+//  contacerta-Briging-Header.h
+//  contacertamobile
+//
+//  Created by Vinicius Galhardo Machado on 06/07/22.
+//
+
+#ifndef contacerta_Briging_Header_h
+#define contacerta_Briging_Header_h
+
+
+#endif /* contacerta_Briging_Header_h */
+
